6a09404dc7ae4a90ab18ea99ef57bade7f0fbec0
Zane Hitchcox <zwhitchcox@gmail.com>
2019-10-23 16:31:37 -0400

    create new post

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,22 @@ pub fn establish_connection() -> PgConnection {
         .expect(&format!("Error connecting to {}", database_url))
 }
 
+use self::models::{Post, NewPost};
+
+pub fn create_post<'a>(conn: &PgConnection, title: &'a str, body: &'a str) -> Post {
+    use schema::posts;
+
+    let new_post = NewPost {
+        title: title,
+        body: body,
+    };
+
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        .get_result(conn)
+        .expect("Error saving new post")
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
